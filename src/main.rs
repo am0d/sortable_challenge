79f13ca398dd234f7dc2d1c8a5e440bfb688046d
Damien Schoof <damien.schoof@gmail.com>
2016-01-16 00:05:53 -0500

    Starting on the actual matching.
    - First attempt is just brute forcing it
      - Search every product to see how many keywods match the listing
      - Then pick the product with the most keywords matching
    
    This finds matches, but seems to miss quite a few that should match, and
    also matches quite a few that shouldn't.  Need to look into that yet.

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,9 @@ struct Product {
     family: String,
     #[serde(rename="announced-date",skip_serializing)]
     announced_date: String,
-    listings: Vec<String>,
+    listings: Vec<Listing>,
+    #[serde(skip_serializing)]
+    keywords: Vec<String>,
 }
 
 #[derive(Serialize,Deserialize,Debug)]
@@ -40,7 +42,7 @@ fn read_objects<T: serde::de::Deserialize>(file_path: &str) -> Result<Vec<T>, So
     try!(f.read_to_string(&mut file_contents));
 
     // map each line in the file to a new object
-    // TODO: work out how to handle errors here???
+    // Note: This just drops any line that has an error
     let all_objects: Vec<T> = file_contents.lines()
                                            .map(|line| serde_json::from_str(&line))
                                            .filter(|o| o.is_ok())
@@ -54,12 +56,48 @@ fn write_objects<T: serde::ser::Serialize>(file_path: &str,
                                            -> Result<(), SortableError> {
     let mut f = try!(File::create(file_path));
 
-    // map each line in the file to a new object
-    // TODO: work out how to handle errors here???
+    // Write each obect on a new line
     for o in all_objects {
         try!(writeln!(f, "{}", serde_json::to_string(o).unwrap()));
     }
     Ok(())
+}
+
+fn match_listings_to_products(all_products: &mut [Product], all_listings: Vec<Listing>) {
+    println!(" -> Preprocessing the products ...");
+    for product in all_products.iter_mut() {
+        // pre-process the keywords
+        product.keywords = format!("{} {} {}",
+                                   product.manufacturer,
+                                   product.family,
+                                   product.model)
+                               .split_whitespace()
+                               .map(|s| String::from(s.to_lowercase()))
+                               .collect();
+        &mut (product.keywords)[..].sort(); // sort the keywords alphabetically
+    }
+    println!(" -> Matching listings ...");
+    for listing in all_listings {
+        let mut best_match: (Option<&mut Product>, usize) = (None, 0);
+        for product in all_products.iter_mut() {
+            let matching_words = (listing.title)[..]
+                                      .split_whitespace()
+                                      .filter(|word| (product.keywords)[..].contains(&String::from(word.to_lowercase())))
+                                      .count();
+            if matching_words == product.keywords.len() {
+                best_match = match best_match {
+                    (None, _) if matching_words > 0 => (Some(product), matching_words),
+                    (_, score) if matching_words > score => (Some(product), matching_words),
+                    _ => best_match,
+                };
+            }
+        }
+
+        match best_match {
+            (Some(ref mut product), _) => product.listings.push(listing),
+            (None, _) => println!("No matching product found for {:?}", listing)
+        }
+    }
 }
 
 fn main() {
@@ -69,7 +107,7 @@ fn main() {
     };
 
     // sort the products by manufacturer
-    &mut all_products[..].sort_by(|p1, p2| p1.manufacturer.cmp(&p2.manufacturer));
+    //&mut all_products[..].sort_by(|p1, p2| p1.manufacturer.cmp(&p2.manufacturer));
 
     println!("Successfully read {} products", all_products.len());
     for p in all_products.iter().take(5) {
@@ -86,5 +124,12 @@ fn main() {
         println!("{:?}", p);
     }
 
+    println!("Matching listings to products ...");
+    match_listings_to_products(&mut all_products[..], all_listings);
+
+    println!("Writing results to file ...");
+    // We call unwrap() as a lazy way to show an error
     write_objects("../results.txt", &all_products[..]).unwrap();
+
+    println!("Done");
 }
