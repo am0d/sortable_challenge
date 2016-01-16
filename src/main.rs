66c990495f25240fa6eb77ce74d518b718863694
Damien Schoof <damien.schoof@gmail.com>
2016-01-15 22:40:21 -0500

    Add code to write the results file
    - Also add sort code to sort products by manufacturer, which I think is
        what I want

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,13 @@ pub mod error;
 #[derive(Serialize,Deserialize,Debug)]
 struct Product {
     product_name: String,
+    #[serde(skip_serializing)]
     manufacturer: String,
+    #[serde(skip_serializing)]
     model: String,
+    #[serde(skip_serializing)]
     family: String,
-    #[serde(rename="announced-date")]
+    #[serde(rename="announced-date",skip_serializing)]
     announced_date: String,
     listings: Vec<String>,
 }
@@ -46,11 +49,27 @@ fn read_objects<T: serde::de::Deserialize>(file_path: &str) -> Result<Vec<T>, So
     Ok(all_objects)
 }
 
+fn write_objects<T: serde::ser::Serialize>(file_path: &str,
+                                           all_objects: &[T])
+                                           -> Result<(), SortableError> {
+    let mut f = try!(File::create(file_path));
+
+    // map each line in the file to a new object
+    // TODO: work out how to handle errors here???
+    for o in all_objects {
+        try!(writeln!(f, "{}", serde_json::to_string(o).unwrap()));
+    }
+    Ok(())
+}
+
 fn main() {
-    let all_products: Vec<Product> = match read_objects("../products.txt") {
+    let mut all_products: Vec<Product> = match read_objects("../products.txt") {
         Ok(products) => products,
         Err(e) => panic!("Error reading products: {}", e),
     };
+
+    // sort the products by manufacturer
+    &mut all_products[..].sort_by(|p1, p2| p1.manufacturer.cmp(&p2.manufacturer));
 
     println!("Successfully read {} products", all_products.len());
     for p in all_products.iter().take(5) {
@@ -66,4 +85,6 @@ fn main() {
     for p in all_listings.iter().take(5) {
         println!("{:?}", p);
     }
+
+    write_objects("../results.txt", &all_products[..]).unwrap();
 }
